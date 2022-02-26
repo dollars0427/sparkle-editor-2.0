f4cc5ff63a10b4e38cbfc9e72d0fb25de9aeb65b
Sardo Ip <sardo@pixelactionstudio.com>
2022-02-26 16:41:56 +0800

    Add editor base

---
+++ src/components/Editor.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="editor">
+    <editor-content :editor="editor" />
+  </div>
+</template>
+
+<script>
+import { Editor, EditorContent } from '@tiptap/vue-2';
+import StarterKit from '@tiptap/starter-kit';
+
+export default {
+  name: 'Editor',
+  components: {
+    EditorContent,
+  },
+  data() {
+    return {
+      editor: null,
+    }
+  },
+  mounted() {
+    this.editor = new Editor({
+      content: '<p>Sparkle Editor 2.0 is coming !</p>',
+      extensions: [
+        StarterKit,
+      ],
+    })
+  },
+
+  beforeDestroy() {
+    this.editor.destroy()
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
