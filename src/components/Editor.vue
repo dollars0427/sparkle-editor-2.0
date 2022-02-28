e7ae9d13e9a71b3a130c2b8b2d6705bf6185a683
Sardo Ip <sardo@pixelactionstudio.com>
2022-02-28 11:20:57 +0800

    Try to make a editor by own

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,21 +1,15 @@
 <template>
-  <div class="editor" :style="style">
-    <editor-content :editor="editor" />
+  <div class="editor-wrapper">
+    <textarea id="editor" :style="style"></textarea>
   </div>
 </template>
 
 <script>
 import Themes from '../default_themes.json';
-
 import { mapState, mapActions } from 'vuex';
-import { Editor, EditorContent } from '@tiptap/vue-2';
-import StarterKit from '@tiptap/starter-kit';
 
 export default {
   name: 'Editor',
-  components: {
-    EditorContent,
-  },
   data(){
     return {
       'currentTheme': '',
@@ -31,21 +25,9 @@ export default {
       this.currentTheme = defaultTheme['id'];
       this.style = `background-image: url('background/${bgImage}');color: ${textColor}`;
     },
-    ...mapActions(['updateEditor'])
   },
   mounted() {
-    const editor = new Editor({
-      content: '<p>Sparkle Editor 2.0 is coming !</p>',
-      extensions: [
-        StarterKit,
-      ],
-    });
     this.loadDefaultTheme();
-    this.updateEditor(editor);
-  },
-  beforeUnmount() {
-    this.editor.destroy();
-    this.updateEditor(editor);
   },
   computed: mapState({
     editor: state => state.editor,
